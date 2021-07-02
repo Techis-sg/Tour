8cd67456ff1f404e01ac9475cb7f631a509d0328
Shobhit <62462717+Techis-sg@users.noreply.github.com>
2021-07-02 19:43:04 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,37 +1,30 @@
-function onClick(element) {
-  document.getElementById("img01").src = element.src;
-  document.getElementById("modal01").style.display = "block";
-}
-
+//form validation function
 (function () {
   'use strict';
   window.addEventListener('load', function () {
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.getElementsByClassName('needs-validation');
-    var input = document.getElementsByClassName('form-control');
-    // Loop over them and prevent submission
-    var validation = Array.prototype.filter.call(forms, function (form) {
-      form.addEventListener('submit', function (event) {
-        if (form.checkValidity() === false) {
-          event.preventDefault();
-          event.stopPropagation();
-        } else if (form.checkValidity() == true) {
-          swal({
-            title: "Message Sent",
-            text: "Thank You!!",
-            icon: "success",
-            button: "OK",
-            html: true,
-            timer: 5000,
-            showCancelButton: true,
-            confirmButtonText: 'Submit',
-            showLoaderOnConfirm: true
-          });
-          // stop form submit only for demo
-          event.preventDefault();
-        }
+
+    form.addEventListener('submit', function (event) {
+      if (form.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
         form.classList.add('was-validated');
-      }, false);
-    });
+      } else {
+        swal({
+          title: "Message Sent",
+          text: "Thank You!!",
+          icon: "success",
+          button: "OK",
+          html: true,
+          timer: 2000,
+          showCancelButton: true,
+          showConfirmButton: false,
+          showLoaderOnConfirm: true
+        });
+        // stop form submit only for demo
+        event.preventDefault();
+        location.replace(location.pathname);
+      }
+
+    }, false);
   }, false);
 })();
